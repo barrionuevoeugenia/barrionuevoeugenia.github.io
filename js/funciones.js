c951816c07ec7fde6f63799bb02f748ca92fa1c3
barrionuevoeugenia <127235397+barrionuevoeugenia@users.noreply.github.com>
2023-03-07 14:08:29 -0300

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,14 @@
+document.getElementById("email").onclick = function (){
+    console.log("Reeplazar");
+    document.getElementById("infocontacto").innerHTML = "e-mail: matinezdebora@gmail.com"
+}
+
+document.getElementById("telefono").onclick = function (){
+    console.log("Reeplazar");
+    document.getElementById("infocontacto").innerHTML = "35109-------"
+}
+
+document.getElementById("linkedin").onclick = function (){
+    console.log("Reeplazar");
+    document.getElementById("infocontacto").innerHTML = "http://ar.linkedin.com/in/deboramartinez"
+}
